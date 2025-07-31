2190a534b484fbba7c45ae5142e1d15a3351e4c6
Eliasld <area_causation872@simplelogin.fr>
2025-07-31 23:46:19 +0200

    Update landing page & main app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Landing from './components/Landing.jsx'
+
 function App() {
   return (
-    <div></div>
+    <div className="bg-neutral-900">
+      <Landing />
+    </div>
   );
 }
 
 export default App;
+
+
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,9 +2,13 @@ import RotatingText from '../React-Bits/RotatingText.jsx'
 
 function Landing() {
   return (
-    <div className="w-full h-screen flex flex-col gap-y-10 justify-center pointer-events-none">
+    <div className="w-full h-screen flex flex-col gap-y-8 items-center justify-center px-1">
       <p className="font-worksans font-bold text-5xl sm:text-8xl text-white text-center">
-        <span className="font-JetBrainsMono text-green-400 italic">$</span> echo "Elias"
+        <span className="font-JetBrainsMono text-green-400 italic">$</span> echo "
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-500 to-purple-500 animate-gradient bg-[length:200%_200%]">
+          Elias
+        </span>
+        "
       </p>
       <div className="flex flex-row gap-x-2 justify-center items-center text-white font-JetBrainsMono text-xl sm:text-3xl font-semibold" >
         <p>Compiling</p>
@@ -21,6 +25,14 @@ function Landing() {
           rotationInterval={2000}
         />
         <p>into code</p>
+      </div>
+      <p className="text-white/60 text-md sm:text-lg text-center max-w-xl font-semibold font-worksans">
+        French lover of Linux, CLIs, video games and Japan.
+      </p>
+      <div className="absolute bottom-5 flex justify-center w-full pointer-events-none">
+        <div className="w-6 h-10 border-2 border-white rounded-full flex items-start justify-center p-1">
+          <div className="w-2 h-2 bg-white rounded-full animate-scroll-down" />
+        </div>
       </div>
     </div >
   );

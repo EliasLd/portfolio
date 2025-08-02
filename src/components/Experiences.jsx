745009866cc3322fef8d053a29c5436f8d920b6f
Eliasld <area_causation872@simplelogin.fr>
2025-08-02 15:44:22 +0200

    Small padding adjustments in About and Experiences components

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -5,7 +5,7 @@ import ExperienceCard from "../components/ExperienceCard";
 
 function Experiences() {
   return (
-    <section id="experiences" className="min-h-screen w-full px-4 py-20 bg-neutral-900 flex justify-center items-center">
+    <section id="experiences" className="min-h-screen w-full px-10 py-20 bg-neutral-900 flex justify-center items-center">
       <div className="max-w-4xl w-full text-white space-y-14">
         <FadeInWhenVisible as="h2" className="text-4xl font-bold text-center font-worksans">
           <DecryptedText text="Experiences" speed={100} animateOn="view" revealDirection="center" />

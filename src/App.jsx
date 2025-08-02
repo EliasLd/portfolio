317c4a68b458f39dd1ecfcd29d025a78b258f60b
Eliasld <area_causation872@simplelogin.fr>
2025-08-03 00:53:56 +0200

    Structure components calls in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import Landing from './components/Landing.jsx'
+import Navbar from './components/Navbar.jsx'
+import About from './components/About.jsx'
+import Technologies from './components/Technologies.jsx'
+import Experiences from './components/Experiences.jsx'
+import Projects from './components/Projects.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
-    <div className="bg-neutral-900">
+    <div >
+      <Navbar />
       <Landing />
+      <About />
+      <Technologies />
+      <Experiences />
+      <Projects />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-
